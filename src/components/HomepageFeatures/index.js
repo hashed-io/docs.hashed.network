71f2fa9470cc4eae9d19b26a2caa800fe21166f5
Max Gravitt <max@digitalscarcity.io>
2022-06-17 16:08:57 -0400

    structuring of sidebar and blog; updates to tokenomics

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,29 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Business Infrastructure',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Hashed Network allows new businesses to stay compliant as a DAO LLC in the State of Wyoming and other jurisdictions
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Secure Bitcoin Treasury',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Manage your treasury using fully native bitcoin with transparent multisignature integrated with your organization's governance
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Order-to-Cash',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Manage invoicing, receivables, and cash flow using triple entry accounting in a manner that protects your privacy
       </>
     ),
   },
