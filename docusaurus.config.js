2777c762b3be5e26ba07b50e2b802a407ecb6a91
Max Gravitt <max@digitalscarcity.io>
2022-06-20 09:23:23 -0400

    creating CNAME file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,6 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hashed-io', // Usually your GitHub org/user name.
+  trailingSlash: false,
   projectName: 'docs.hashed.network', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
