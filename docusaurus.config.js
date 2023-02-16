9129c936137bc63d5013a11f20472e33175c8fe0
tlacloc <desneruda@gmail.com>
2023-02-16 17:33:41 -0600

    add mermaid for docsaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,12 @@ const config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
-
+  themes: ['@docusaurus/theme-mermaid'],
+  // In order for Mermaid code blocks in Markdown to work,
+  // you also need to enable the Remark plugin with this option
+  markdown: {
+    mermaid: true,
+  },
   presets: [
     [
       'classic',
